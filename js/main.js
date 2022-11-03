df5960e443433e3f5c0c2350abfb426fd386a9df
shara <nastena1801@gmail.com>
2022-11-03 23:29:12 +0300

    removed clear form

--- js/main.js
+++ js/main.js
@@ -8,12 +8,8 @@ $(function(){
         $('.popup-container').css('display', 'none');
     };
 
-    let clearForm = function(){
-        $('.popup-container > input[type="text"]').val('');
-    };
-
+    
     $('.close').on('click', function(){
-        clearForm();
         closePopup();
     });
 });
